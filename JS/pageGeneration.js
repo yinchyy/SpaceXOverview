ac39a345a6f9b03f43f3f3cf428813f07830e3d7
Szymon Studniarz <szymek1019@gmail.com>
2022-02-07 18:59:06 +0100

    create generator class with method, generate main menu

---
+++ JS/pageGeneration.js
@@ -0,0 +1,24 @@
+class pageGeneration{
+    static generatePage(target) {
+        const container = document.getElementsByClassName("content")[0];
+        if (target.toLowerCase() === "main") {
+            container.innerHTML = `
+            <div id="menuName"class="menuElements">
+            <p>Menu</p>
+            </div>
+            <hr>
+            <div class="menuElements">
+            <a class="categoryButton" onclick="pageGeneration.generatePage('starlinks')">Starlinks</a>
+            <a class="categoryButton" onclick="pageGeneration.generatePage('rockets')">Rockets</a>
+            <a class="categoryButton" onclick="pageGeneration.generatePage('tesla')">Tesla in space</a>
+            </div>
+        `;
+        }
+        else if (target.toLowerCase() === "starlinks") {
+            container.innerHTML = ``;
+        }
+        else {
+            pageGeneration.generatePage("main");
+        }
+    }
+};
