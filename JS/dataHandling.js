340abf0de227af399a94810a3bc43a652edd7187
Szymon Studniarz <szymek1019@gmail.com>
2022-02-08 14:07:49 +0100

    add pagination, static variable to store fetched data

--- JS/dataHandling.js
+++ JS/dataHandling.js
@@ -1,27 +1,33 @@
 class dataHandling{
+    static data;
     static async getData(target) {
         try {
             const response = await fetch(`https://api.spacexdata.com/v4/${target}`);
             const data = await response.json();
-            dataHandling.printData(data);
+            dataHandling.data = data;
+            dataHandling.printData(data,0);
             return data;
             }
         catch (err) {
             console.alert("Fetching data failed...");
         }
     }
-    static printData(data) {
-        let i = 0;
-            for (let elem of data) {
-                document.getElementById("itemsContainer").innerHTML += `
+    static printData(data, pageNumber) {
+        const itemsContainer = document.getElementById('itemsContainer');
+        itemsContainer.innerHTML = "";
+        let maxIndex = (pageNumber * 10) + 9;
+        let elemIndex = 0 + (pageNumber*10);
+        if (maxIndex >= data.length) {
+            maxIndex = data.length - 1;
+            elemIndex = maxIndex - (maxIndex % 10);
+        }
+        for (elemIndex; elemIndex <= maxIndex;++elemIndex) {
+                itemsContainer.innerHTML += `
                 <div class="resultItem">
-                <p>${elem.spaceTrack.OBJECT_ID}</p>
-                <p>${elem.spaceTrack.OBJECT_NAME}</p>
-                <p>${elem.spaceTrack.LAUNCH_DATE}</p>
+                <p>${data[elemIndex].spaceTrack.OBJECT_ID}</p>
+                <p>${data[elemIndex].spaceTrack.OBJECT_NAME}</p>
+                <p>${data[elemIndex].spaceTrack.LAUNCH_DATE}</p>
                 </div>`;
-                if (++i >= 10) {
-                    break;
-                }
             }
     }
 };
