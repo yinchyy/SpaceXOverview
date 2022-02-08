c1391ece2b0fd330340ede7e5188af08a1cabdde
Szymon Studniarz <szymek1019@gmail.com>
2022-02-08 10:21:37 +0100

    add method to fetch and print data on page
    One method receives data from server and passes it to another method,
    which displays it on the website in proper place, in new rows. At the
    time being only first 10 rows are being displayed, pagination needs to
    be implemented.

---
+++ JS/dataHandling.js
@@ -0,0 +1,25 @@
+class dataHandling{
+    static async getData(target) {
+        try {
+            const response = await fetch(`https://api.spacexdata.com/v4/${target}`);
+            const data = await response.json();
+            dataHandling.printData(data);
+            return data;
+            }
+        catch (err) {
+            console.alert("Fetching data failed...");
+        }
+    }
+    static printData(data) {
+        let i = 0;
+            for (let elem of data) {
+                document.getElementById("itemsContainer").innerHTML += `
+                <div class="resultItem">
+                <p>${elem.spaceTrack.OBJECT_NAME}</p>
+                </div>`;
+                if (++i >= 10) {
+                    break;
+                }
+            }
+    }
+};
