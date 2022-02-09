2bf92447f8a67ff8c96fdd6c786173aa268aa187
Szymon Studniarz <szymek1019@gmail.com>
2022-02-09 19:57:53 +0100

    add page navigation with current page display and start/end jump
    Navigation buttons become inactive when movement is impossible (on first
    or last page), also jump buttons are inactive if there is only one-page
    movement possible, so for example jumping to first page is possible only
    when active page is 3 or higher.

--- JS/pageGeneration.js
+++ JS/pageGeneration.js
@@ -40,10 +40,51 @@ class pageGeneration{
     }
     static generatePageNav(start,end) {
         const navContainer = document.getElementById('pageNav');
-        navContainer.innerHTML+=`
-        <a class="navButton" onclick="pageGeneration.generatePage('main')">&lt;</a>
-        <a class="navButton" onclick="pageGeneration.generatePage('main')">&lt;</a>
-        <a class="navButton" onclick="pageGeneration.generatePage('main')">&lt;</a>
-        `
+        navContainer.innerHTML = '';
+        if (start > 0) {
+            if (start > 1) {
+                navContainer.innerHTML += `
+                <a class="navButton" onclick="dataHandling.printData(dataHandling.data,0)">&lt;&lt;</a>
+                 `;    
+            }
+            else {
+                navContainer.innerHTML += `
+                <a class="navButton inactive" onclick="">&lt;&lt;</a>
+                `;
+            }
+        navContainer.innerHTML += `
+        <a class="navButton" onclick="dataHandling.printData(dataHandling.data,${start - 1})">&lt;</a>
+        `;
+        }
+        else {
+             navContainer.innerHTML += `
+             <a class="navButton inactive" onclick="">&lt;&lt;</a>
+             <a class="navButton inactive" onclick="">&lt;</a>
+            `;
+        }
+        navContainer.innerHTML += `
+            <a class="navButton inactive" onclick="">${start+1}</a>
+         `;
+        if (start < end) {
+            navContainer.innerHTML += `
+            <a class="navButton" onclick="dataHandling.printData(dataHandling.data,${start + 1})">&gt;</a>
+            `;
+            if (start < end - 1) {
+                navContainer.innerHTML += `
+                <a class="navButton" onclick="dataHandling.printData(dataHandling.data,${end})">&gt;&gt;</a>
+                `;       
+            }
+            else {
+                navContainer.innerHTML += `
+                <a class="navButton inactive" onclick="">&gt;&gt;</a>
+                `;          
+            }
+        }
+        else {
+          navContainer.innerHTML += `
+            <a class="navButton inactive" onclick="">&gt;</a>
+            <a class="navButton inactive" onclick="dataHandling.printData(dataHandling.data,${end})">&gt;&gt;</a>
+         `;   
+        }
     }
 };
