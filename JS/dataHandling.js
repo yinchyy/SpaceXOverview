acfe6bf386cde85c2bdfb5ae36ab60c7c3f511f9
Szymon Studniarz <szymek1019@gmail.com>
2022-02-10 22:45:59 +0100

    add page target check in printData method

--- JS/dataHandling.js
+++ JS/dataHandling.js
@@ -5,14 +5,14 @@ class dataHandling{
             const response = await fetch(`https://api.spacexdata.com/v4/${target}`);
             const data = await response.json();
             dataHandling.data = data;
-            dataHandling.printData(data,0);
+            dataHandling.printData(data,0,target);
             return data;
             }
         catch (err) {
             console.alert("Fetching data failed...");
         }
     }
-    static printData(data, pageNumber) {
+    static printData(data, pageNumber,target) {
         const itemsContainer = document.getElementById('itemsContainer');
         itemsContainer.innerHTML = "";
         let maxIndex = (pageNumber * 10) + 9;
@@ -22,13 +22,25 @@ class dataHandling{
             elemIndex = maxIndex - (maxIndex % 10);
         }
         let className = "resultItem";
-        for (elemIndex; elemIndex <= maxIndex; ++elemIndex) {
+        if (target === "starlink") {   
+            for (elemIndex; elemIndex <= maxIndex; ++elemIndex) {
                 itemsContainer.innerHTML += `
                 <div class="${className}">
                 <p>${data[elemIndex].spaceTrack.OBJECT_ID}</p>
                 <p>${data[elemIndex].spaceTrack.OBJECT_NAME}</p>
                 <p>${data[elemIndex].spaceTrack.LAUNCH_DATE}</p>
                 </div>`;
+            }
+        }
+        else if (target === "rockets") {
+            for (elemIndex; elemIndex <= maxIndex; ++elemIndex) {
+                itemsContainer.innerHTML += `
+                <div class="${className}">
+                <p>${data[elemIndex].name}</p>
+                <p>${data[elemIndex].first_flight}</p>
+                <p>${data[elemIndex].active}</p>
+                </div>`;
+            }
         }
         pageGeneration.generatePageNav(pageNumber, Math.floor(data.length/10));
     }
